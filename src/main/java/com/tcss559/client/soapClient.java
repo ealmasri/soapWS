9f773d69ba415f701dc9ecc9c5685cb05d9a7184
orvidia <orvidia@orvidia-PC>
2021-10-17 23:10:39 -0700

    12

--- src/main/java/com/tcss559/client/soapClient.java
+++ src/main/java/com/tcss559/client/soapClient.java
@@ -2,21 +2,18 @@ package com.tcss559.client;
 import com.tcss559.*;
 import java.net.URL;
 import javax.xml.namespace.QName;
-import jakarta.xml.ws.Service;
-// comm
-//ent
-
+import javax.xml.ws.Service;
 
 
 public class soapClient {
     public static void main(String[] args) throws Exception {
     	 // create wsdl url
-        URL wsdlDocumentUrl = new URL("http://localhost:9010/ws/calc-soap?wsdl");
+        URL wsdlDocumentUrl = new URL("http://0.0.0.0/calcService?wsdl");
         QName calcSoapService = new QName("http://calc-soap/ws", "CalcSoapService");
         // create web service
         Service service = Service.create(wsdlDocumentUrl, calcSoapService);
         // get object of pointed service port
-        Calculator calc1 = service.getPort(Calculator.class);
+        calcWebService calc1 = service.getPort(calcWebService.class);
         // testing request
         System.out.println(calc1.multiply(2, 453350));
     }
